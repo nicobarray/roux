0fb0df464523e96609099364566d4f42ac713aca
Nicolas Barray <nbarray@gmail.com>
2020-02-09 03:06:20 +0100

    Format files

--- Vector.cs
+++ Vector.cs
@@ -1,11 +1,15 @@
 using UnityEngine;
 
-namespace Roux {
-    public static class Vector {
-        public static Vector2 Xy(this Vector3 vector) {
+namespace Roux
+{
+    public static class Vector
+    {
+        public static Vector2 Xy(this Vector3 vector)
+        {
             return new Vector2(vector.x, vector.y);
         }
-        public static Vector2 Xz(this Vector3 vector) {
+        public static Vector2 Xz(this Vector3 vector)
+        {
             return new Vector2(vector.x, vector.z);
         }
     }
